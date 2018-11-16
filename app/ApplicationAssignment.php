8b8160988163061446ab37ec7959070f0bab0f5b
Sermar De Sousa <sermarruiz@gmail.com>
2018-11-16 09:52:42 -0200

    Fixes application assignment model

--- app/ApplicationAssignment.php
+++ app/ApplicationAssignment.php
@@ -4,15 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class application_assignment extends Model
+class ApplicationAssignment extends Model
 {
     protected $table = 'application_assignments';
 
     protected $guarded = ['id', 'created_at', 'update_at'];
 
+    
+    // Protect table fields
+    protected $fillable = [
+        'flag_ok', 'application_id', 'service_id'
+    ];
 
     // Protect table sorted fields
     protected $sorted = [
-        'flag_ok'
+        'flag_ok', 'application_id', 'service_id'
     ];
 }
