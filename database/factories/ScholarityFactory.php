3a5e99a9cfa37cbcd276c40fedf5257d17c89544
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2018-11-16 15:53:58 -0200

    Fixes faker type text

--- database/factories/ScholarityFactory.php
+++ database/factories/ScholarityFactory.php
@@ -9,7 +9,7 @@ $factory->define(Scholarity::class, function (Faker $faker) {
         'end_date' => Carbon::now()->addYears(5),
         'init_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
         'link' => $faker->url,
-        'scholarity_type' => $faker->text($maxNbChars = 20),
+        'scholarity_type' => $faker->text($maxNbChars = 150),
         'teaching_institution' => $faker->numberBetween($min = 1, $max = 4),
         'candidate_id' => $faker->numberBetween($min = 1, $max = 100),
     ];
