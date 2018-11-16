0717ff648d688ed4cecee3aa8f5f6175e92c84ee
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2018-11-16 13:49:54 -0200

    Fixes seeder name

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,9 +6,9 @@ class DatabaseSeeder extends Seeder {
 
     public function run() {
 
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
         $this->call(ApplicationsTableSeeder::class);
-        $this->call(ApplicationCriterionsTableSeeder::class);
+        $this->call(ApplicationCriteriaTableSeeder::class);
         // $this->call(ApplicationDocumentsTableSeeder::class);
         $this->call(EdictsTableSeeder::class);
         $this->call(VacanciesTableSeeder::class);
