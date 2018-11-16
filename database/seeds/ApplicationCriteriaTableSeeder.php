5eaba6068bd5b23f26fc23ebcbead8f5c9d91520
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2018-11-16 10:07:27 -0200

    Fixes name class application criteria

--- database/seeds/ApplicationCriteriaTableSeeder.php
+++ database/seeds/ApplicationCriteriaTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class ApplicationCriterionsTableSeeder extends Seeder
+class ApplicationCriteriaTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
