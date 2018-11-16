0d89478059d07e8682d4e7b5dafb7c634d1cde4c
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2018-11-16 13:44:56 -0200

    Fixes application string values.

--- database/factories/ApplicationFactory.php
+++ database/factories/ApplicationFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Application::class, function (Faker $faker) {
     return [
-        'status' => $faker->text($maxNbChars = 50), 
+        'status' => $faker->text($maxNbChars = 30), 
         'observation' => $faker->text($maxNbChars = 100), 
         'candidate_id' => $faker->numberBetween($min = 1, $max = 100),
         'vacancy_id' => $faker->numberBetween($min = 1, $max = 100),
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,8 +7,8 @@ class DatabaseSeeder extends Seeder {
     public function run() {
 
         $this->call(UsersTableSeeder::class);
-        // $this->call(ApplicationsTableSeeder::class);
-        // $this->call(ApplicationCriterionsTableSeeder::class);
+        $this->call(ApplicationsTableSeeder::class);
+        $this->call(ApplicationCriterionsTableSeeder::class);
         // $this->call(ApplicationDocumentsTableSeeder::class);
         $this->call(EdictsTableSeeder::class);
         $this->call(VacanciesTableSeeder::class);
