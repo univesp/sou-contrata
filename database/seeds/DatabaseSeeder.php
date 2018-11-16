199923942e5b86b3c95659fcdc2ac49832cef4d2
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2018-11-16 15:05:14 -0200

    Fixes service factory call model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,13 +6,13 @@ class DatabaseSeeder extends Seeder {
 
     public function run() {
 
-        // $this->call(UsersTableSeeder::class);
-        $this->call(ApplicationsTableSeeder::class);
-        $this->call(ApplicationCriteriaTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        // $this->call(ApplicationsTableSeeder::class);
+        // $this->call(ApplicationCriteriaTableSeeder::class);
         // $this->call(ApplicationDocumentsTableSeeder::class);
         $this->call(EdictsTableSeeder::class);
         $this->call(ServiceSeeder::class);
-        $this->call(ApplicationAssignmentSeeder::class);
+        // $this->call(ApplicationAssignmentSeeder::class);
         $this->call(VacanciesTableSeeder::class);
         $this->call(CriteriaTableSeeder::class);
         $this->call(VacancyCriteriaTableSeeder::class);
