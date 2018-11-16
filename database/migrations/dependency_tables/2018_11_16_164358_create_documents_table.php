4bb7dcf89d32788442322d60314a89bd9bc2d0fd
eduardo oliveira <eduardooliveira@eduardos-MacBook-Pro.local>
2018-11-16 18:33:13 -0200

    Criando a Table of Document

--- database/migrations/dependency_tables/2018_11_16_164358_create_documents_table.php
+++ database/migrations/dependency_tables/2018_11_16_164358_create_documents_table.php
@@ -10,11 +10,15 @@ class CreateDocumentsTable extends Migration {
 
         Schema::create('documents', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('candidate_id')->unsined();
+            $table->integer('candidate_id')->unsigned()->index();
+            $table->foreign('candidate_id')->references('id')->on('candidates');
             $table->string('document_type', 20);
             $table->string('number',50);
             $table->string('number_serie',10);
-            $table->date('date_emission');
+            $table->date('date_issue');
+            $table->string('uf_issue', 2);
+            $table->string('zone', 6);
+            $table->string('section', 6);
             $table->timestamps();
         });
     }
