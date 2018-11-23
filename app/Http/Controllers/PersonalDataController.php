1e9d81f02170db6a381ab0913b5fececf1e12c1a
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2018-11-23 11:05:25 -0200

    Validate if file military exist

--- app/Http/Controllers/PersonalDataController.php
+++ app/Http/Controllers/PersonalDataController.php
@@ -45,8 +45,15 @@ class PersonalDataController extends Controller
         // Envio dos documentos para o Storage
         $path_file_rg = $request['file_rg']->store("documents-rg/{$user_id}");
         $path_file_title = $request['file_title']->store("documents-title/{$user_id}");
-        $path_file_military = $request['file_military']->store("documents-military/{$user_id}");
-
+        $path_file_military = '';
+        
+        // Validate if file military exist
+        if (empty($request['file_military'])) {
+            $path_file_military = 'Empty';
+        } else {
+            $path_file_military = $request['file_military']->store("documents-military/{$user_id}");
+        }
+        
         // Validate all fields
         $validator = Validator::make($request->input(), [
             // Candidate validations
@@ -61,9 +68,9 @@ class PersonalDataController extends Controller
 
             // Documents validations
             'elector_title'             => 'required',
-            'elector_link'              => 'required',
+            // 'file_title'                => 'required',
             'rg_number'                 => 'required',
-            'number_link'               => 'required',
+            // 'file_rg'                   => 'required',
             'date_issue'                => 'required',
             'uf_issue'                  => 'required',
 
@@ -109,11 +116,11 @@ class PersonalDataController extends Controller
                 // Create new document
                 $document = new Document();
                 $document->elector_title            = $request->elector_title;
-                $document->elector_link             = $request->elector_link;
+                $document->elector_link             = $path_file_title;
                 $document->military_certificate     = $request->military_certificate;
-                $document->military_link            = $request->military_link;
+                $document->military_link            = $path_file_military;
                 $document->number                   = $request->number;
-                $document->number_link              = $request->number_link;
+                $document->number_link              = $path_file_rg;
                 $document->date_issue               = date('Y-m-d', strtotime($request->date_issue));
                 $document->uf_issue                 = $request->uf_issue;
                 $document->zone                     = 'Empty';
