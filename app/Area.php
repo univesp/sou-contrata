bac0a50715d9268745094d6f8896bca19d8ab5b5
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2018-11-29 10:45:59 -0200

    Create models to academic data view

---
+++ app/Area.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Area extends Model
+{
+    // Protect table name
+    protected $table = 'applications';
+
+    // Protected guarded table fields
+    protected $guarded = ['id', 'created_at', 'update_at'];
+
+    // Protect table fields
+    protected $fillable = [
+        'description'
+    ];
+
+    // Protect table sorted fields
+    protected $sorted = [
+        'description'
+    ];
+
+    // Relationships
+    public function scholarityAreas()
+    {
+        return $this->hasMany(ScholarityArea::class);
+    }
+
+    public function areaSubareas()
+    {
+        return $this->hasMany(AreaSubarea::class);
+    }
+}
--- app/Scholarity.php
+++ app/Scholarity.php
@@ -20,7 +20,8 @@ class Scholarity extends Model
         'link',
         'scholarity_type',
         'teaching_institution',
-        'candidate_id'
+        'candidate_id',
+        // 'area_id'
     ];
 
     // Protect table sorted fields
@@ -32,11 +33,17 @@ class Scholarity extends Model
         'scholarity_type',
         'teaching_institution',
         'candidate_id',
+        // 'area_id'
     ];
 
     // Relationships
     public function candidate()
     {
         return $this->belongsTo(Candidate::class);
+    }
+
+    public function scholarityAreas()
+    {
+        return $this->hasMany(ScholarityArea::class);
     }
 }
