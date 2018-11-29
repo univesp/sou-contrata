82979407db642dbf8cd3d553fb344746eb307651
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2018-11-29 09:32:25 -0200

    Fixes fields on candidates controller & view

--- app/Http/Controllers/PersonalDataController.php
+++ app/Http/Controllers/PersonalDataController.php
@@ -72,7 +72,7 @@ class PersonalDataController extends Controller
         // Validate all fields
         $validator = Validator::make($request->all(), [
             // Candidate validations
-            'celular'           => 'required',
+            'mobile'            => 'required',
             'cpf'               => 'required|unique:candidates',
             'file_cpf'          => 'required',
             'date_birth'        => 'required',
@@ -130,8 +130,8 @@ class PersonalDataController extends Controller
             $candidate->curriculum_link     = isset($request->curriculum_link)? $request->curriculum_link: 'Empty';
             $candidate->obs_deficient       = isset($request->obs_deficient)? $request->obs_deficient: 'Empty';
             $candidate->flag_deficient      = ($request->obs_deficient) ? 1 : 0 ;
-            $candidate->phone               = trim($request->cod_phone.' '.$request->phone);
-            $candidate->celular             = trim($request->cod_mobile.' '.$request->celular);
+            $candidate->phone               = trim($request->area_phone.' '.$request->phone);
+            $candidate->mobile              = trim($request->area_mobile.' '.$request->mobile);
             $candidate->user_id             = $user_id;
 
             // Save in database
