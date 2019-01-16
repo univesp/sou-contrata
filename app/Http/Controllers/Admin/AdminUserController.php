03c76a3d87e6a542103adef228098327b5b53705
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2019-01-16 10:33:53 -0200

    Add list function (BACK)

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use App\Candidate;
+use DB;
 
 class AdminUserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // Example id
-        $id = 225;
+        // List all users
+        $users = User::all();
 
         // Return this view
         return view("admin.admin-user.index")
-            ->with('id', $id);
+            ->with('users', $users);
     }
 }
