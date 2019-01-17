f83896c77c85a839922f518475beddd3b2e2e15f
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2019-01-17 11:19:07 -0200

    Fixes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -35,8 +35,7 @@
                     <div class="col-sm-12 col-xs-12 col-md-10">
                         <input type="hidden" value="{{ $id = Session::get('user')['id'] }}">
                         @if (Session::get('user')['user'])
-                            <span class="float-right text-user"><a href="{{route('admin/personal-data/edit', $id)}}" class="profile-link" data-toggle="tooltip" data-placement="bottom" title="Perfil">@yield('username')</a></span>
-                            <span class="float-right text-user"><a href="{{ url('admin/admin-user') }}" class="profile-link user-list-icon" data-toggle="tooltip" data-placement="bottom" title="Usuários"><i class="fa fa-users"></i></a></span>
+                            <span class="float-right text-user">@yield('username')</span>
                         @endif
                     </div>
                     @if (Session::get('user')['user'])
