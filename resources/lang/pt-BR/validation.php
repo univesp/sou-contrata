dc03d56fc5e719c901aaa3634b6ed74a1649c7bd
Sermar De Sousa <sermar.sousa@connectcom.univesp.br>
2019-01-31 10:23:50 -0200

    Translate english to portugues variables

--- resources/lang/pt-BR/validation.php
+++ resources/lang/pt-BR/validation.php
@@ -160,6 +160,11 @@ return [
         'public_place'                  => 'Lougradouro',
         'state'                         => 'UF',
         'type_public_place'             => 'Tipo Logradouro',
+
+        // Password
+        'password'                      => 'Senha',
+        'old_password'                  => 'Senha Antiga',
+        'password_confirm'              => 'Confirmação de Senha'
     ],
 
 ];
